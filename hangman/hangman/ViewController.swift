dc3edd7659820fa762dd83d2946e5b3c4e4ef8c2
dfreire <dario.freire@gmail.com>
2014-10-04 13:33:10 +0100

    created project on xcode

---
+++ hangman/hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hangman
+//
+//  Created by Dario Freire on 04/10/14.
+//  Copyright (c) 2014 dfreire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
